3cc8a90b11f3a556a310f0bbd1a8e53c3efa3bbc
llyp618 <llyp618@163.com>
2018-01-26 10:29:18 +0800

    space

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-	watch: 'true',
-	devtool: "#cheap-module-eval-source-map",
-	module: {
-		rules: [{
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-			loader: 'babel-loader',
-			options: {
-				presets: [
-					['env', {
-						modules: false,
-						targets: {
-							browsers: ["last 2 versions", "not ie <= 8"]
-						}
-						}]
-					],
-				plugins: [
-					"transform-runtime"
-				]
-			}
-		}
-		}]
-	}
+  watch: 'true',
+  devtool: "#cheap-module-eval-source-map",
+  module: {
+    rules: [{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['env', {
+            modules: false,
+            targets: {
+              browsers: ["last 2 versions", "not ie <= 8"]
+            }
+            }]
+          ],
+        plugins: [
+          "transform-runtime"
+        ]
+      }
+    }
+    }]
+  }
 }
